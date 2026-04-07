f7cfe688291860ee621b1b6d0304e84aa371de71
Priya_Tiwari <priyatiwari.dev16@gmail.com>
2026-04-08 03:46:15 +0530

    added uv.lock

--- server/app.py
+++ server/app.py
@@ -19,3 +19,10 @@ app = create_fastapi_app(
 
 # Mount API
 # app.mount("/web", inner_app)
+
+def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+if __name__ == "__main__":
+    main()
