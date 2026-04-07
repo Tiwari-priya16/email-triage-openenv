9eb6752a3696df2fcb29829bd6fffd1060b043f8
Priya_Tiwari <priyatiwari.dev16@gmail.com>
2026-04-08 01:25:19 +0530

    fixed app.py

--- server/app.py
+++ server/app.py
@@ -1,9 +1,15 @@
+from fastapi import FastAPI
 from openenv.core.env_server import create_fastapi_app
 from models import EmailObservation, TriageAction
 from server.email_triage_environment import EmailTriageEnvironment
 
-app = create_fastapi_app(
+# Create OpenEnv app
+inner_app = create_fastapi_app(
     EmailTriageEnvironment,
     TriageAction,
     EmailObservation,
 )
+
+# Mount it under /web (REQUIRED FOR HF)
+app = FastAPI()
+app.mount("/web", inner_app)
